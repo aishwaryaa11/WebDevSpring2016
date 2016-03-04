d7073495d8e31016d7b62e74da27aaafe1139b3f
Aishwarya Afzulpurkar <aishwaryaa11@hotmail.com>
2016-03-03 22:35:38 -0500

    header,sidebar,home,app and config.js

---
+++ public/project/config.js
@@ -0,0 +1,35 @@
+(function(){
+    angular
+        .module("Travelogue")
+        .config(configuration);
+
+    function configuration($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl: "views/users/login.view.html",
+                controller: "LoginController"
+            })
+            .when("/profile", {
+                templateUrl: "views/users/profile.view.html",
+                controller: "ProfileController"
+            })
+            .when("/admin", {
+                templateUrl: "views/admin/admin.view.html"
+            })
+            .when("/home", {
+                templateUrl: "views/home/home.view.html"
+            })
+            .when("/diary", {
+                templateUrl: "views/diary/diary.view.html",
+                controller: "DiaryController"
+            })
+            .when("/diary-expand", {
+                templateUrl: "views/diary/diary-expand.view.html"
+            })
+            .when("/photos", {
+                templateUrl: "views/photos/photos.view.html",
+                controller: "PhotosController"
+            })
+            .otherwise("/home")
+    }
+})();
