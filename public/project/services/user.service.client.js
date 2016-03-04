e7e5ef84b18b51df537b4830d91e1262246003d9
Aishwarya Afzulpurkar <aishwaryaa11@hotmail.com>
2016-03-03 23:14:21 -0500

    user service client

---
+++ public/project/services/user.service.client.js
@@ -0,0 +1,110 @@
+(function() {
+    angular
+        .module("Travelogue")
+        .factory("UserService", userService);
+
+    function userService() {
+        var users = [
+            {	"_id":123, "firstName":"Alice",            "lastName":"Wonderland",
+                "username":"alice",  "password":"alice",   "roles": ["student"],
+                "email": "alice@alice.com"                                              },
+            {	"_id":234, "firstName":"Bob",              "lastName":"Hope",
+                "username":"bob",    "password":"bob",     "roles": ["admin"],
+                "email": "bob@bob.com"                                                  },
+            {	"_id":345, "firstName":"Charlie",          "lastName":"Brown",
+                "username":"charlie","password":"charlie", "roles": ["faculty"],
+                "email": "charlie@charlie.como"                                         },
+            {	"_id":456, "firstName":"Dan",              "lastName":"Craig",
+                "username":"dan",    "password":"dan",     "roles": ["faculty", "admin"],
+                "email": "dan@dan.com"                                                  },
+            {	"_id":567, "firstName":"Edward",           "lastName":"Norton",
+                "username":"ed",     "password":"ed",      "roles": ["student"],
+                "email": "ed@ed.com"                                                    }
+        ];
+
+        var service = {
+            findUserByCredentials: findUserByCredentials,
+            findUserById: findUserById,
+            findUserByUsername: findUserByUsername,
+            findAllUsers: findAllUsers,
+            createUser: createUser,
+            deleteUserById: deleteUserById,
+            updateUser: updateUser
+        };
+
+        return service;
+
+        function findUserByCredentials(username, password, callback) {
+            for (var u in users) {
+                if (users[u].username == username && users[u].password == password) {
+                    return users[u];
+                }
+            }
+
+            return null;
+        }
+
+        function findUserById(userId, callback) {
+            for (var u in users){
+                if (users[u]._id == userId) {
+                    return users[u];
+                }
+            }
+
+            return null;
+        }
+
+        function findUserByUsername(username, callback) {
+            for (var u in users){
+                if (users[u].username == username) {
+                    return users[u];
+                }
+            }
+
+            return null;
+        }
+
+        function findAllUsers(callback) {
+            return users;
+        }
+
+        function createUser(user, callback) {
+            var newUser = {
+                _id: (new Date).getTime(),
+                username: user.username,
+                password: user.password,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                roles: user.roles,
+                email: user.email
+            };
+
+            users.push(newUser);
+            return newUser;
+        }
+
+        function deleteUserById(userId, callback) {
+            var user = findUserById(userId);
+            if (user != null) {
+                users.splice(users.indexOf(user), 1);
+            } else {
+                return null;
+            }
+        }
+
+        function updateUser(userId, user, callback) {
+            var userTemp = findUserById(userId);
+            if (userTemp != null) {
+                userTemp.firstName = user.firstName;
+                userTemp.lastName = user.lastName;
+                userTemp.password = user.password;
+                userTemp.username = user.username;
+                userTemp.roles = user.roles;
+                userTemp.email = user.email;
+                return userTemp;
+            } else {
+                return null;
+            }
+        }
+    }
+})();
