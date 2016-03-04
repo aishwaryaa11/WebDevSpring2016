dae9b485a1deb0781a6a1d04f6bdd8ed88e86c44
Aishwarya Afzulpurkar <aishwaryaa11@hotmail.com>
2016-03-03 22:56:59 -0500

    picture and link changes

---
+++ public/project/views/users/login.controller.js
@@ -0,0 +1,25 @@
+(function(){
+    angular
+        .module("Travelogue")
+        .controller("LoginController", loginController);
+
+    function loginController($scope, UserService, $rootScope, $location) {
+        $scope.message = null;
+        $scope.login = login;
+
+        function login(user) {
+            var userTemp = UserService.findUserByCredentials(user.username, user.password);
+            if (userTemp != null) {
+                $rootScope.currentUser = userTemp;
+                if (userTemp.roles != null && userTemp.roles.indexOf("admin") >= 0) {
+                    $location.url("/admin");
+                } else {
+                    $location.url("/profile");
+                }
+            } else {
+                $scope.message = "Cannot find user. Try again.";
+                return ;
+            }
+        }
+    }
+})();
