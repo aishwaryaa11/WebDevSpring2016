2974312917dbff4cb08ae220875bc9788aeb7217
Aishwarya Afzulpurkar <aishwaryaa11@hotmail.com>
2016-03-25 19:58:32 -0400

    server

--- server.js
+++ server.js
@@ -1,6 +1,46 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3020;
+var cookie_secret = "xsf4662817";
+
+function rootRequest (req, res) {
+    res.send('hello world')
+}
+
+// create a default connection string
+var connectionString = 'mongodb://127.0.0.1:27017/webdev2016';
+
+// use remote connection string
+// if running in remote server
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+    secret: cookie_secret,
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.get('/hello', rootRequest);
+
+require("./public/assignment/server/app.js")(app, db, mongoose);
+require("./public/project/server/app.js")(app);
+
 app.listen(port, ipaddress);
