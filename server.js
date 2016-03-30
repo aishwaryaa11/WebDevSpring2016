6e39f024d9273e680ff5ff327574d93c52bae4cd
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-29 21:26:09 -0400

    required and configured the body parser and multer modules

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer');
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
@@ -10,7 +12,9 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: { secure: true }
-}))
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./public/assignment/server/app.js")(app);
 
