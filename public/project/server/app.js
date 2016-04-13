21c8a5b49d1236d300188f7dcae1e97267e4b416
Aishwarya Afzulpurkar <aishwaryaa11@hotmail.com>
2016-04-13 10:51:17 -0400

    project server app.js

--- public/project/server/app.js
+++ public/project/server/app.js
@@ -6,7 +6,7 @@ module.exports = function(app, uuid, mongoose, db) {
     var userService = require("./services/user.service.server.js")(app, userModel);
     var diaryService = require("./services/diary.service.server.js")(app, userModel, diaryModel);
     var diaryExpandService = require("./services/diary-expand.service.server.js")(app, diaryModel);
-    var photoService = require("./services/photo.service.server.js")(app, userModel, photoModel);
-    var photoExpandService = require("./services/photo-expand.service.server.js")(app, photoModel);
+    //var photoService = require("./services/photo.service.server.js")(app, userModel, photoModel);
+    //var photoExpandService = require("./services/photo-expand.service.server.js")(app, photoModel);
 
 };
