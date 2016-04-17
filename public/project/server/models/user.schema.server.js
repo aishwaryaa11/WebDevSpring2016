ebde715bfa47fd0abf9b68d51a2afe6e264feae9
Aishwarya Afzulpurkar <aishwaryaa11@hotmail.com>
2016-04-17 18:17:29 -0400

    userschema username is unique check

--- public/project/server/models/user.schema.server.js
+++ public/project/server/models/user.schema.server.js
@@ -1,7 +1,7 @@
 module.exports = function(mongoose) {
 
     var UserTSchema =mongoose.Schema({
-        username: String,
+        username: {type: String, unique: true},
         password: String,
         firstName: String,
         lastName: String,
