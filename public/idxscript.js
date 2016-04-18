ef2e64942dad5f6784d464f1d4b4c533c467fbb6
Aishwarya Afzulpurkar <aishwaryaa11@hotmail.com>
2016-04-18 02:25:39 -0400

    homepage

--- public/idxscript.js
+++ public/idxscript.js
@@ -2,8 +2,9 @@ angular.module('homepage', ['ui.bootstrap']).controller('mycontroller',['$scope'
 
     $scope.slides = [];
     $scope.slides.push({title: 'Project Proposal', text: 'Use cases, functionality description and client-server model for Travelogue', image: 'http://placekitten.com/300/200', link: "https://docs.google.com/a/husky.neu.edu/document/d/1eTukKILfFtT6AdwE6gftETqZjoGU96EA0YGm2OxVHk0/edit?usp=sharing"});
-    $scope.slides.push({title: 'Project', text: 'Link to the Project homepage', image: 'http://placekitten.com/301/200', link: 'project/client'});
-    $scope.slides.push({text: 'Assignments', text: 'Link to the Assignments homepage', image: 'http://placekitten.com/302/200', link: 'assignment/client'});
+    $scope.slides.push({title: 'Travelogue', text: 'Link to the Project homepage', image: 'http://placekitten.com/301/200', link: 'project/client'});
+    $scope.slides.push({title: 'Assignments', text: 'Link to the Assignments homepage', image: 'http://placekitten.com/302/200', link: 'assignment/client'});
+    $scope.slides.push({title: 'Github Repo', text: 'Check out my code here', image: 'http://placekitten.com/302/200', link: 'http://github.com/aishwaryaa11/webdev'});
 
     $scope.setActive = function(idx) {
         $scope.slides[idx].active=true;
